ff1191e6e7011dbd1d228512ecf76cbfa7d1d657
eafoam <mmmm3732@naver.com>
2023-01-16 15:32:42 +0900

    주석

---
+++ app/config/db.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "kk6641kk",
+    DB: "test_db"
+};
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res)=>{
+    res.json({message: "Hello World!"});
+});
+
+
+require("./app/routes/customer.routes.js")(app);
+
+// 포트넘버 설정
+app.listen(3000, ()=>{
+    console.log("Server is running on port 3000.");
+})
+
+
+
+
+
